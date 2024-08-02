f6b41e603d40c9a6d49202bd0fa436d852e557d9
lesnikovskiy <lesnikovski@gmail.com>
2024-08-02 23:54:35 +0300

    Add enum and complex matching description()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+#[derive(Debug)]
+enum Media {
+    Book {
+        title: String,
+        author: String,
+    },
+    Movie {
+        title: String,
+        director: String,
+    },
+    Audiobook {
+        title: String,
+    },
+}
+
+impl Media {
+    fn description(&self) -> String {
+        if let Media::Book { title, author } = self {
+            format!("Book: {} {}", title, author)
+        } else if let Media::Movie { title, director } = self {
+            format!("Movie: {} {}", title, director)
+        } else if let Media::Audiobook { title } = self {
+            format!("Audiobook: {}", title)
+        } else {
+            String::from("Media description")
+        }
+    }
+}
+
+fn print_media(media: &Media) {
+    println!("{:#?}", media);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let audiobook = Media::Audiobook { title: String::from("An Audiobook") };
+
+    let good_movie = Media::Movie {
+        title: String::from("Good Moview"),
+        director: String::from("Director"),
+    };
+
+    let bad_book = Media::Book {
+        title: String::from("Bad Book"),
+        author: String::from("Bad Author"),
+    };
+
+    audiobook.description();
+    good_movie.description();
+    bad_book.description();
+
+    print_media(&audiobook);
+    print_media(&good_movie);
+    print_media(&bad_book);
 }
