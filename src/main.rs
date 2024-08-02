5effefaa751caae8fcdfead3b6127d42d1ed4be0
lesnikovskiy <lesnikovski@gmail.com>
2024-08-03 00:00:47 +0300

    Use match enum pattern

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,19 @@ enum Media {
 
 impl Media {
     fn description(&self) -> String {
-        if let Media::Book { title, author } = self {
-            format!("Book: {} {}", title, author)
-        } else if let Media::Movie { title, director } = self {
-            format!("Movie: {} {}", title, director)
-        } else if let Media::Audiobook { title } = self {
-            format!("Audiobook: {}", title)
-        } else {
-            String::from("Media description")
+        // if let Media::Book { title, author } = self {
+        //     format!("Book: {} {}", title, author)
+        // } else if let Media::Movie { title, director } = self {
+        //     format!("Movie: {} {}", title, director)
+        // } else if let Media::Audiobook { title } = self {
+        //     format!("Audiobook: {}", title)
+        // } else {
+        //     String::from("Media description")
+        // }
+        match self {
+            Media::Book { title, author } => { format!("Book: {} {}", title, author) }
+            Media::Movie { title, director } => { format!("Movie: {} {}", title, director) }
+            Media::Audiobook { title } => { format!("Audiobook: {}", title) }
         }
     }
 }
